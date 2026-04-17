3637975a4597c19cbe3a116629954281e139fc7b
Daniele Bartolini <dbartolini@crownengine.org>
2026-04-17 23:32:28 +0200

    device: warn on invalid keys

--- src/device/boot_config.cpp
+++ src/device/boot_config.cpp
@@ -21,7 +21,7 @@ namespace crown
 BootConfig::BootConfig(Allocator &a)
 	: boot_script_name(a)
 	, boot_package_name(u64(0))
-	, render_config_name(u64(0))
+	, render_config_name(STRING_ID_64("core/renderer/default", UINT64_C (0x1b92f3ff7ca4157c)))
 	, window_title(a)
 	, save_dir(a)
 	, user_config(a)
@@ -52,7 +52,11 @@ static void parse_physics(PhysicsSettings *settings, const char *json)
 		} else if (cur->first == "max_substeps") {
 			settings->max_substeps = sjson::parse_int(cur->second);
 		} else {
-			logw(BOOT_CONFIG, "Unknown physics property '%s'", cur->second);
+			logw(BOOT_CONFIG
+				, "Unknown physics property '%.*s'"
+				, cur->first.length()
+				, cur->first.data()
+				);
 		}
 	}
 }
@@ -63,25 +67,36 @@ static void parse_renderer_settings(BootConfig *config, const char *json)
 	JsonObject renderer(ta);
 	sjson::parse(renderer, json);
 
-	if (json_object::has(renderer, "resolution")) {
-		JsonArray resolution(ta);
-		sjson::parse_array(resolution, renderer["resolution"]);
-		config->window_w = sjson::parse_int(resolution[0]);
-		config->window_h = sjson::parse_int(resolution[1]);
+	auto cur = json_object::begin(renderer);
+	auto end = json_object::end(renderer);
+	for (; cur != end; ++cur) {
+		JSON_OBJECT_SKIP_HOLE(renderer, cur);
+
+		if (cur->first == "resolution") {
+			JsonArray resolution(ta);
+			sjson::parse_array(resolution, cur->second);
+			config->window_w = sjson::parse_int(resolution[0]);
+			config->window_h = sjson::parse_int(resolution[1]);
+		} else if (cur->first == "aspect_ratio") {
+			config->aspect_ratio = sjson::parse_float(cur->second);
+		} else if (cur->first == "device_id") {
+			DynamicString hex(ta);
+			sjson::parse_string(hex, cur->second);
+			s64 id;
+			from_hex(id, hex.c_str());
+			config->device_id = (u16)id;
+		} else if (cur->first == "vsync") {
+			config->vsync = sjson::parse_bool(cur->second);
+		} else if (cur->first == "fullscreen") {
+			config->fullscreen = sjson::parse_bool(cur->second);
+		} else {
+			logw(BOOT_CONFIG
+				, "Unknown renderer property '%.*s'"
+				, cur->first.length()
+				, cur->first.data()
+				);
+		}
 	}
-	if (json_object::has(renderer, "aspect_ratio"))
-		config->aspect_ratio = sjson::parse_float(renderer["aspect_ratio"]);
-	if (json_object::has(renderer, "device_id")) {
-		DynamicString hex(ta);
-		sjson::parse_string(hex, renderer["device_id"]);
-		s64 id;
-		from_hex(id, hex.c_str());
-		config->device_id = (u16)id;
-	}
-	if (json_object::has(renderer, "vsync"))
-		config->vsync = sjson::parse_bool(renderer["vsync"]);
-	if (json_object::has(renderer, "fullscreen"))
-		config->fullscreen = sjson::parse_bool(renderer["fullscreen"]);
 }
 
 static void parse_platform_settings(BootConfig *config, const char *json)
@@ -90,14 +105,36 @@ static void parse_platform_settings(BootConfig *config, const char *json)
 	JsonObject platform(ta);
 	sjson::parse(platform, json);
 
-	if (json_object::has(platform, "save_dir"))
-		sjson::parse_string(config->save_dir, platform["save_dir"]);
+	auto cur = json_object::begin(platform);
+	auto end = json_object::end(platform);
+	for (; cur != end; ++cur) {
+		JSON_OBJECT_SKIP_HOLE(platform, cur);
 
-	if (json_object::has(platform, "renderer"))
-		parse_renderer_settings(config, platform["renderer"]);
+		if (cur->first == "save_dir") {
+			sjson::parse_string(config->save_dir, cur->second);
+		} else if (cur->first == "renderer") {
+			parse_renderer_settings(config, cur->second);
+		} else if (cur->first == "render_settings") {
+			render_settings::parse(config->render_settings, cur->second);
+		} else {
+			logw(BOOT_CONFIG
+				, "Unknown platform property '%.*s'"
+				, cur->first.length()
+				, cur->first.data()
+				);
+		}
+	}
+}
 
-	if (json_object::has(platform, "render_settings"))
-		render_settings::parse(config->render_settings, platform["render_settings"]);
+static bool is_platform_name(const StringView &key)
+{
+	return key == "android"
+		|| key == "html5"
+		|| key == "ios"
+		|| key == "linux"
+		|| key == "osx"
+		|| key == "windows"
+		;
 }
 
 bool BootConfig::parse(const char *json)
@@ -106,31 +143,37 @@ bool BootConfig::parse(const char *json)
 	JsonObject cfg(ta);
 	sjson::parse(cfg, json);
 
-	// General configs
-	sjson::parse_string(boot_script_name, cfg["boot_script"]);
-	boot_package_name = sjson::parse_resource_name(cfg["boot_package"]);
+	auto cur = json_object::begin(cfg);
+	auto end = json_object::end(cfg);
+	for (; cur != end; ++cur) {
+		JSON_OBJECT_SKIP_HOLE(cfg, cur);
 
-	if (json_object::has(cfg, "window_title"))
-		sjson::parse_string(window_title, cfg["window_title"]);
-
-	if (json_object::has(cfg, "render_config")) {
-		render_config_name = sjson::parse_resource_name(cfg["render_config"]);
-	} else {
-		render_config_name = STRING_ID_64("core/renderer/default", UINT64_C(0x1b92f3ff7ca4157c));
+		if (cur->first == "boot_script") {
+			sjson::parse_string(boot_script_name, cur->second);
+		} else if (cur->first == "boot_package") {
+			boot_package_name = sjson::parse_resource_name(cur->second);
+		} else if (cur->first == "window_title") {
+			sjson::parse_string(window_title, cur->second);
+		} else if (cur->first == "render_config") {
+			render_config_name = sjson::parse_resource_name(cur->second);
+		} else if (cur->first == "physics") {
+			parse_physics(&physics_settings, cur->second);
+		} else if (cur->first == "render_settings") {
+			render_settings::parse(render_settings, cur->second);
+		} else if (cur->first == "user_config") {
+			sjson::parse_string(user_config, cur->second);
+		} else if (cur->first == CROWN_PLATFORM_NAME) {
+			parse_platform_settings(this, cur->second);
+		} else {
+			if (!is_platform_name(cur->first)) {
+				logw(BOOT_CONFIG
+					, "Unknown boot_config property '%.*s'"
+					, cur->first.length()
+					, cur->first.data()
+					);
+			}
+		}
 	}
-
-	if (json_object::has(cfg, "physics"))
-		parse_physics(&physics_settings, cfg["physics"]);
-
-	if (json_object::has(cfg, "render_settings"))
-		render_settings::parse(render_settings, cfg["render_settings"]);
-
-	if (json_object::has(cfg, "user_config"))
-		sjson::parse_string(user_config, cfg["user_config"]);
-
-	// Platform-specific configs.
-	if (json_object::has(cfg, CROWN_PLATFORM_NAME))
-		parse_platform_settings(this, cfg[CROWN_PLATFORM_NAME]);
 
 	return true;
 }
